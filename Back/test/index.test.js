c39b9f6a370dc9d637dad82707298f8166e3f5d9
alex <lxalonso@gmail.com>
2022-09-22 12:05:26 +0200

    test de prueba ok

--- Back/test/index.test.js
+++ Back/test/index.test.js
@@ -1,5 +1,19 @@
-const supertest = require('supertest')
+const  mongoose = require('mongoose');
+const supertest = require('supertest');
+const { dbConnection } = require('../db/config');
+const { app , server } = require('../index');
+
+beforeAll(async()=>{
+    await dbConnection()
+    
+    
+})
 
 test('primer test',()=>{
 
+})
+
+afterAll(()=>{
+    server.close()
+    mongoose.connection.close()
 })
